feb9f8a8ac336d9719df1805b008a6b3be70df88
Fer Alva <feralvaa@hotmail.com>
2020-07-15 20:56:48 -0400

    array of words

--- react-passport-mongo/client/src/components/ResultList.js
+++ react-passport-mongo/client/src/components/ResultList.js
@@ -1,16 +1,23 @@
-import React from "react";
-import Card from "@material-ui/core/Card";
+import React from 'react';
+import Card from '@material-ui/core/Card';
 
 function ResultList(props) {
-  return (
-    <Card className="list-group">
-      {props.results.map(result => (
-        <li className="list-group-item" key={result.id}>
-          <img alt={result.title} className="img-fluid" src={result.images.original.url} />
-        </li>
-      ))}
-    </Card>
-  );
+	return (
+		<Card className='list-group'>
+			{props.results.map(result => {
+				console.log(result.data.data);
+				return result.data.data.map(element => (
+					<li className='list-group-item' key={element.id}>
+						<img
+							alt={element.title}
+							className='img-fluid'
+							src={element.images.original.url}
+						/>
+					</li>
+				));
+			})}
+		</Card>
+	);
 }
 
 export default ResultList;
